5ff0b0d5e56c0dcdc4b26c7a178dfc8e163e9774
Whizfarsh <oyefarsh@gmail.com>
2025-07-12 22:09:51 +0100

    styled similar

--- src/features/recipes/SimilarRecipes.jsx
+++ src/features/recipes/SimilarRecipes.jsx
@@ -2,7 +2,14 @@ import styled from "styled-components";
 import Meals from "../../ui/Meals";
 import { useRecipes } from "../../context/RecipesContext";
 
-const StyledSimilarRecipe = styled.div``;
+const StyledSimilarRecipe = styled.div`
+	margin-top: 5rem;
+	h3 {
+		font-size: 2.4rem;
+		margin-bottom: -1rem;
+		text-transform: Capitalize;
+	}
+`;
 
 function SimilarRecipes() {
 	const { similar } = useRecipes();
