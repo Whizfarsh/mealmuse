5a18efa0fad5c7e4272696ded58bcb61be74ea7a
Whizfarsh <oyefarsh@gmail.com>
2025-07-14 13:29:35 +0100

    effect for fetching recipe was added

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,120 @@
+import styled from "styled-components";
+import AddIngrredients from "../ui/AddIngrredients";
+import HeaderMenu from "../ui/HeaderMenu";
+import Footer from "../ui/Footer";
+import AddedIngredients from "../ui/AddedIngredients";
+import { useEffect, useRef, useState } from "react";
+import { useGlobal } from "../context/GlobalContext";
+import { useIngredients } from "../context/IngredientsContext";
+
+const StyledDashBoard = styled.div`
+	flex: 1;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+
+	margin-top: 2.4rem;
+	padding: 3.2rem;
+`;
+
+const StyledPage = styled.div`
+	overflow: hidden;
+
+	display: flex;
+	flex-direction: column;
+	min-height: 100vh;
+
+	position: relative;
+`;
+
+const SearchButton = styled.button`
+	margin-top: 2.4rem;
+	padding: 1.2rem 2.4rem;
+	cursor: pointer;
+	border: none;
+	border-radius: 0.8rem;
+
+	display: " block";
+	background-color: var(--dark-2);
+	color: var(--light-0);
+`;
+
+function Dashboard() {
+	const [toSearch, setToSearch] = useState(false);
+
+	const addIngRefs = useRef();
+
+	const { API_Key } = useGlobal();
+
+	const { ingredients, addedIng } = useIngredients();
+
+	function handleScroll() {
+		if (addIngRefs.current) {
+			addIngRefs.current.scrollIntoView({
+				behavior: "smooth",
+				block: "start",
+			});
+		}
+	}
+
+	function handleSearch() {
+		setToSearch(true);
+	}
+
+	useEffect(
+		function () {
+			if (!toSearch) return;
+			async function getRecipeByIngredients() {
+				// if (!ingredients) return;
+				// if (addedIng.length === 0) return;
+
+				if (addedIng.length === 0) {
+					// setRecipeResults([]);
+					return;
+				}
+
+				try {
+					const res = await fetch(
+						`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${API_Key}&ingredients=${ingredients}&number=5 `
+					);
+					const data = await res.json();
+
+					const allIds = data.map((recipe) => recipe.id).join(",");
+
+					const bulkRes = await fetch(
+						`https://api.spoonacular.com/recipes/informationBulk?ids=${allIds}&includeNutrition=true&apiKey=${API_Key}`
+					);
+
+					const bulkRecipeData = await bulkRes.json();
+
+					// console.log(data);
+					console.log(allIds);
+					console.log(bulkRecipeData);
+					// setRecipeResults(data);
+					// console.log(recipeResults);
+				} catch (err) {
+					console.log(err.message);
+				}
+			}
+
+			getRecipeByIngredients();
+		},
+		[API_Key, ingredients, addedIng, toSearch]
+	);
+
+	// const [i]
+	return (
+		<StyledPage>
+			<HeaderMenu bgColor="var(--light-1)" />
+			<StyledDashBoard ref={addIngRefs}>
+				<AddIngrredients />
+				<SearchButton onClick={handleSearch}>Search</SearchButton>
+			</StyledDashBoard>
+			<AddedIngredients handleClick={handleScroll} />
+			<Footer />
+		</StyledPage>
+	);
+}
+
+export default Dashboard;
