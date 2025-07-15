f347a1352e2c77bc4a6acccc7e75b8d42ad5cb4a
Whizfarsh <oyefarsh@gmail.com>
2025-07-15 19:29:22 +0100

    added some variations for the loginout button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,8 +2,8 @@ import styled, { css } from "styled-components";
 
 const variations = {
 	primary: css`
-		border-radius: 1.8rem;
 		background-color: var(--dark-2);
+		border-radius: 1.8rem;
 		color: #fff;
 		font-weight: 600;
 		padding: 1.2rem;
@@ -15,6 +15,15 @@ const variations = {
 			background-color: var(--light-1);
 			color: var(--dark-2);
 		}
+	`,
+	loginout: css`
+		background-color: var(--dark-3);
+		color: var(--light-0);
+		border: none;
+		padding: 0.7rem 1.4rem;
+		border-radius: 0.7rem;
+		font-weight: 600;
+		cursor: pointer;
 	`,
 };
 
