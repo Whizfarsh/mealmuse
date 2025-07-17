e57c6b7640a481ba565da8140c633a014179d20d
Whizfarsh <oyefarsh@gmail.com>
2025-07-17 22:27:16 +0100

    added some additional data

--- src/features/recipes/apiRecipe.js
+++ src/features/recipes/apiRecipe.js
@@ -4,3 +4,56 @@ import { API_Key } from "../../context/GlobalContext";
 
 // const { API_Key } = useGlobal();
 export const recipeBaseUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_Key}`;
+
+const cuisine = [
+	"African",
+	"Asian",
+	"American",
+	"British",
+	"Cajun",
+	"Caribbean",
+	"Chinese",
+	"Eastern European",
+	"European",
+	"French",
+	"German",
+	"Greek",
+	"Indian",
+	"Irish",
+	"Italian",
+	"Japanese",
+	"Jewish",
+	"Korean",
+	"Latin American",
+	"Mediterranean",
+	"Mexican",
+	"Middle Eastern",
+	"Nordic",
+	"Southern",
+	"Spanish",
+	"Thai",
+	"Vietnamese",
+];
+const dietsList = [
+	"Gluten Free",
+	"Ketogenic",
+	"Lacto-Vegetarian",
+	"Ovo-Vegetarian",
+	"Pescetarian",
+	"Vegan",
+	"Vegetarian",
+];
+const recipeTypesList = [
+	"Main Course",
+	"Side Dish",
+	"Appetizer",
+	"Dessert",
+	"Salad",
+	"Beverage",
+	"Breakfast",
+	"Brunch",
+	"Sauce",
+	"Snack",
+];
+
+export { cuisine, dietsList, recipeTypesList };
