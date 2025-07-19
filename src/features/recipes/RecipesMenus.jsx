2f28fae63a874d71e8b83a2c19ebbb0b94a61a59
Whizfarsh <oyefarsh@gmail.com>
2025-07-19 12:59:12 +0100

    import and making use of other recioes source

--- src/features/recipes/RecipesMenus.jsx
+++ src/features/recipes/RecipesMenus.jsx
@@ -1,11 +1,12 @@
 import Meals from "../../ui/Meals";
 
 import styled from "styled-components";
-import { useIngredients } from "../../context/IngredientsContext";
 import { Link } from "react-router-dom";
+import Recipefilters from "./Recipefilters";
+import { useRecipes } from "../../context/RecipesContext";
 
 const RecipeMenus = styled.div`
-	margin: 1.2rem 1.8rem;
+	margin: -1em 1.8rem 0rem;
 `;
 
 const NoRecipe = styled.div`
@@ -29,19 +30,24 @@ const SearchLink = styled(Link)`
 function RecipesMenus() {
 	document.title = "MealMuse | Recipes";
 
-	const { recipeResults } = useIngredients();
+	const { localRecipes } = useRecipes();
 
 	return (
-		<RecipeMenus>
-			{recipeResults.length > 0 ? (
-				<Meals title="" recipes={recipeResults} />
-			) : (
-				<NoRecipe className="no-recipes">
-					<p>No recipes found. Please add ingredients to search for recipes.</p>
-					<SearchLink to="/">Search your recipe</SearchLink>
-				</NoRecipe>
-			)}
-		</RecipeMenus>
+		<>
+			<Recipefilters />
+			<RecipeMenus>
+				{localRecipes.length > 0 ? (
+					<Meals title="" recipes={localRecipes} />
+				) : (
+					<NoRecipe className="no-recipes">
+						<p>
+							No recipes found. Please add ingredients to search for recipes.
+						</p>
+						<SearchLink to="/">Search your recipe</SearchLink>
+					</NoRecipe>
+				)}
+			</RecipeMenus>
+		</>
 	);
 }
 
