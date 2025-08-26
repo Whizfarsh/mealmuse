f72893c05533004354236d04c3908e45ed6881b2
Whizfarsh <oyefarsh@gmail.com>
2025-08-26 14:30:08 +0100

    cleaned

--- client/src/features/recipes/RecipesMenus.jsx
+++ client/src/features/recipes/RecipesMenus.jsx
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 import Recipefilters from "./Recipefilters";
 import { useRecipes } from "../../context/RecipesContext";
 import { useState } from "react";
-// import { useFilter } from "../../context/FilterContext";
 
 const RecipeMenus = styled.div`
 	margin: -1em 1.8rem 0rem;
@@ -33,63 +32,7 @@ function RecipesMenus() {
 	document.title = "MealMuse | Recipes";
 
 	const [sortBy, setSortBy] = useState("none");
-	// const { selectedCuisine, selectedDiet, selectedType, duration } = useFilter();
-
-	// const { localRecipes } = useRecipes();
 	const { recipes } = useRecipes();
-
-	console.log(recipes);
-
-	// const recipesToDisplay = localRecipes.filter((recipe) => {
-	// 	const selectedCuisineLower = selectedCuisine?.toLowerCase();
-	// 	const selectedDietLower = selectedDiet?.toLowerCase();
-	// 	const selectedTypeLower = selectedType?.toLowerCase();
-
-	// 	const cuisines = recipe.cuisines?.map((c) => c.toLowerCase()) || [];
-	// 	const diets = recipe.diets?.map((d) => d.toLowerCase()) || [];
-	// 	const types = recipe.dishTypes?.map((t) => t.toLowerCase()) || [];
-
-	// 	const matchesCuisine =
-	// 		!selectedCuisine ||
-	// 		selectedCuisineLower === "all" ||
-	// 		cuisines.includes(selectedCuisineLower);
-
-	// 	const matchesDiet =
-	// 		!selectedDiet ||
-	// 		selectedDietLower === "all" ||
-	// 		diets.includes(selectedDietLower);
-
-	// 	const matchesType =
-	// 		!selectedType ||
-	// 		selectedTypeLower === "all" ||
-	// 		types.includes(selectedTypeLower);
-
-	// 	const matchesDuration =
-	// 		!duration ||
-	// 		duration === "all" ||
-	// 		(recipe.readyInMinutes <= 15 && duration === "quick") ||
-	// 		(recipe.readyInMinutes > 15 &&
-	// 			recipe.readyInMinutes <= 30 &&
-	// 			duration === "short") ||
-	// 		(recipe.readyInMinutes > 30 &&
-	// 			recipe.readyInMinutes <= 60 &&
-	// 			duration === "medium") ||
-	// 		(recipe.readyInMinutes > 60 && duration === "long");
-
-	// 	return matchesCuisine && matchesDiet && matchesType && matchesDuration;
-	// });
-
-	// const sortedRecipes = [...recipesToDisplay].sort((a, b) => {
-	// 	if (sortBy === "name") {
-	// 		return a.title.localeCompare(b.title);
-	// 	} else if (sortBy === "duration") {
-	// 		return a.readyInMinutes - b.readyInMinutes;
-	// 	} else if (sortBy === "servings") {
-	// 		return a.servings - b.servings;
-	// 	} else {
-	// 		return recipesToDisplay;
-	// 	}
-	// });
 
 	return (
 		<>
