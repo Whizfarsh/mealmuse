b07dda973d8dc54f9200b410bf8cf96a324c16ca
Whizfarsh <oyefarsh@gmail.com>
2025-09-28 11:59:04 +0100

    removed some lines and added allDiets and allCuisines

--- client/src/context/GlobalContext.jsx
+++ client/src/context/GlobalContext.jsx
@@ -1,16 +1,12 @@
 /* eslint-disable react/prop-types */
 import { createContext, useContext, useState } from "react";
-
-// export const API_Key = "ae0fc98731a34ef1a9dd2aff9448752b";
-export const API_Key = "c6414a7b8638405386aee74a1cee23e2";
-// export const API_Key = "1f390112146b438c9fff53abe569e602";
-// export const API_Key = "58f1453b84d44ea5a09a1bfe25c4a436";
-// export const API_Key = "";
+import useFetch from "../../hooks/useFetch";
 
 const GlobalContext = createContext();
 
 function GlobalProvider({ children }) {
 	const [showMbMenu, setShowMbMenu] = useState(false);
+
 	const convertMinutes = function (minutesGiven) {
 		const hour = Math.ceil(minutesGiven / 60);
 		const minutes = minutesGiven % 60;
@@ -18,20 +14,17 @@ function GlobalProvider({ children }) {
 		return `${hour}h:${minutes < 10 ? minutes + "0" : minutes} `;
 	};
 
-	const [favorites, setFavorites] = useState(() => {
-		const getFavorites = localStorage.getItem("favorites");
-		return getFavorites ? JSON.parse(getFavorites) : [];
-	});
+	const { data: allDiets } = useFetch("/api/v1/diets");
+	const { data: allCusines } = useFetch("/api/v1/cuisines");
 
 	return (
 		<GlobalContext.Provider
 			value={{
-				API_Key,
-				favorites,
-				setFavorites,
 				convertMinutes,
 				showMbMenu,
 				setShowMbMenu,
+				allDiets,
+				allCusines,
 			}}
 		>
 			{children}
